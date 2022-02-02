102f7a2961dc90b6294a0ebd68ae032a93e8cacb
Xiuling Pan <panx110825@gmail.com>
2022-02-02 12:26:05 -0500

    ss

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-    app.use('api', createProxyMiddleware({ target: 'https://myapp-boss-server.herokuapp.com/', changeOrigin: true }));
+    app.use(createProxyMiddleware('/', { target: 'https://myapp-boss-server.herokuapp.com/', changeOrigin: true }));
 };
